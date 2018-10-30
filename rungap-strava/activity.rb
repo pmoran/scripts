1ff9825ef9ea42521dc3907cdcbeadcda4f7d243
Peter Moran <peterlmoran@gmail.com>
2018-10-30 22:11:53 +1100

    spaces!

--- rungap-strava/activity.rb
+++ rungap-strava/activity.rb
@@ -5,44 +5,43 @@ require 'json'
 
 class Activity
 
-	
-	EXPORT_DIR = "/tmp/strava_export"
+    EXPORT_DIR = "/tmp/strava_export"
 
-	def initialize
-		@client = Strava::Api::V3::Client.new(:access_token => ENV["access_token"])
-	end
+    def initialize
+        @client = Strava::Api::V3::Client.new(:access_token => ENV["access_token"])
+    end
 
-	def create_activities
-		Dir.glob("#{EXPORT_DIR}/*.json").each do |f|
-			data = JSON.parse(File.read(f))
-			activity = build_activity(data)
-			if activity
-				puts activity
-				@client.create_an_activity(activity)
-			else
-				puts "Not a running activity"
-			end
-		end
+    def create_activities
+        Dir.glob("#{EXPORT_DIR}/*.json").each do |f|
+            data = JSON.parse(File.read(f))
+            activity = build_activity(data)
+            if activity
+                puts activity
+                @client.create_an_activity(activity)
+            else
+                puts "Not a running activity"
+            end
+        end
 
-	end
+    end
 
-	private
+    private
 
-	def build_activity(data)
-		return nil unless (data["activityType"]["internalName"]) == "Running"
-		{ name: data["title"], 
-				description: data["description"],
-				type: 'run',
-				start_date_local: data["startTime"]["time"],
-				elapsed_time: data["elapsedTime"].to_i,
-				distance: data["distance"] }
-	end
+    def build_activity(data)
+        return nil unless (data["activityType"]["internalName"]) == "Running"
+        { name: data["title"], 
+                description: data["description"],
+                type: 'run',
+                start_date_local: data["startTime"]["time"],
+                elapsed_time: data["elapsedTime"].to_i,
+                distance: data["distance"] }
+    end
 
 end
 
 
 if __FILE__ == $0
 
-	Activity.new.create_activities
+    Activity.new.create_activities
 
 end
