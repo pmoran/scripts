a0cf6728c913cff43b132d8de85360f39a2034bd
Peter Moran <peterlmoran@gmail.com>
2018-10-30 22:06:53 +1100

    First cut

---
+++ rungap-strava/activity.rb
@@ -0,0 +1,48 @@
+require 'strava/api/v3'
+require 'json'
+
+# https://yizeng.me/2017/01/11/get-a-strava-api-access-token-with-write-permission/
+
+class Activity
+
+	
+	EXPORT_DIR = "/tmp/strava_export"
+
+	def initialize
+		@client = Strava::Api::V3::Client.new(:access_token => ENV["access_token"])
+	end
+
+	def create_activities
+		Dir.glob("#{EXPORT_DIR}/*.json").each do |f|
+			data = JSON.parse(File.read(f))
+			activity = build_activity(data)
+			if activity
+				puts activity
+				@client.create_an_activity(activity)
+			else
+				puts "Not a running activity"
+			end
+		end
+
+	end
+=
+	private
+
+	def build_activity(data)
+		return nil unless (data["activityType"]["internalName"]) == "Running"
+		{ name: data["title"], 
+				description: data["description"],
+				type: 'run',
+				start_date_local: data["startTime"]["time"],
+				elapsed_time: data["elapsedTime"].to_i,
+				distance: data["distance"] }
+	end
+
+end
+
+
+if __FILE__ == $0
+
+	Activity.new.create_activities
+
+end
---
+++ rungap-strava/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf /tmp/strava_export/
+mkdir /tmp/strava_export
+unzip -o '*.zip' -d /tmp/strava_export
